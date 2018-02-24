dd23197954709bdd4b7ba8655b0eeb1b1d9766a6
Isobel Redelmeier <iredelmeier@gmail.com>
2018-02-23 17:45:40 -0800

    Add crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! # serde-helpers
+//!
+//! `serde-helpers` provides wrappers for serializing and deserializing data structures
+//! to and from certain data formats.
+//!
+//! The library is primarily intended to keep crates that need to serialize or deserialize
+//! data from having to be aware of `serde` itself, if they do not otherwise have a need
+//! to consume it.
+//!
+//! Additionally, `serde-helpers` provides some additional consistency across data formats,
+//! e.g., by providing a single, consolidated `Error` struct that wraps the format-specific
+//! error.
 extern crate serde;
 #[cfg(feature = "serde_json")]
 extern crate serde_json;
